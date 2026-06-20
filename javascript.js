cff40c16416ec72a0d6c41a8f6fd16c42f76d15e
Howard <howard.zhang0108@gmail.com>
2026-06-20 14:14:33 -0400

    Fix getComputerChoice function
    Forgot to add the brackets to the math.random
    function, and also made the conditionals more
    robust. Function now properly returns
    rock, paper, or scissors.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 let computerChoice = "no input"
 
 function getComputerChoice() {
-    let choice = Math.random;
+    let choice = Math.random();
     if (choice <= 0.33) {
         computerChoice = "rock";
-    } else if (choice <= 0.66) {
+    } else if (choice > 0.33 && choice <= 0.66) {
         computerChoice = "paper";
-    } else {
+    } else if (choice > 0.66) {
         computerChoice = "scissors";
     }
 }
