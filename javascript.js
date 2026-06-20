90f6b4e7dd47b90b9e362b3497de2dbca50d2807
Howard <howard.zhang0108@gmail.com>
2026-06-20 14:21:38 -0400

    Add getHumanChoice function
    This function will ask for a simple input and
    assume the input is valid.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 let computerChoice = "no input"
+let humanChoice = "no input"
 
 function getComputerChoice() {
     let choice = Math.random();
@@ -9,4 +10,8 @@ function getComputerChoice() {
     } else if (choice > 0.66) {
         computerChoice = "scissors";
     }
+}
+
+function getHumanChoice() {
+    humanChoice = prompt(What will you play?);
 }
