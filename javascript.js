314a04e98bb5ec42571dfef1785397031bf434e2
Howard <howard.zhang0108@gmail.com>
2026-06-20 13:50:45 -0400

    Add getComputerChoice function
    This function will generate a random number and
    assign rock, paper, or scissors as the computer
    choice depending on the number.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function getComputerChoice {
+    let choice = Math.random;
+    if (choice <= 0.33) {
+        choice = "rock";
+    } else if (choice <= 0.66) {
+        choice = "paper";
+    } else {
+        choice = "scissors";
+    }
+}
